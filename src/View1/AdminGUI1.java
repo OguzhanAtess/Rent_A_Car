ccaa81caf176e56b54a0d288520a64d7ecf3fade
OguzhanAtess <oguzhn.atess@gmail.com>
2024-01-01 03:05:53 +0300

    Rent a Car projesi

---
+++ src/View1/AdminGUI1.java
@@ -0,0 +1,316 @@
+package View1;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.event.TableModelEvent;
+import javax.swing.event.TableModelListener;
+import javax.swing.table.DefaultTableModel;
+
+import Model.Admin;
+
+import java.awt.Color;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JTabbedPane;
+import javax.swing.JScrollPane;
+import javax.swing.JTextField;
+import javax.swing.JTable;
+import java.awt.Font;
+import java.sql.SQLException;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class AdminGUI1 extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+	
+	static Admin admin = new Admin();
+	private JTextField fld_brand;
+	private JTextField fld_gear;
+	private JTextField fld_fuel;
+	private JTextField fld_capacity;
+	private JTextField fld_deposit;
+	private JTextField fld_rental_fee;
+	private JTable table_cars;
+	private DefaultTableModel carModel = null;
+	private Object[] carData = null;
+	private JTextField fld_car_id;
+	private JTextField fld_car_count;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					AdminGUI1 frame = new AdminGUI1(admin);
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 * @throws SQLException 
+	 */
+	public AdminGUI1(Admin admin) throws SQLException {
+		setTitle("Admin Sistemi");
+		
+		carModel = new DefaultTableModel();
+		Object[] colCarName = new Object[8];
+		colCarName[0] = "ID";
+		colCarName[1] = "ARABA";
+		colCarName[2] = "VİTES";
+		colCarName[3] = "YAKIT";
+		colCarName[4] = "KAPASİTE";
+		colCarName[5] = "DEPOZİTO";
+		colCarName[6] = "KİRALAMA ÜCRETİ";
+		colCarName[7] = "ARAÇ SAYISI";
+		carModel.setColumnIdentifiers(colCarName);
+		carData = new Object[8];
+		for(int i = 0; i < admin.getCarList().size(); i++) {
+			carData[0] = admin.getCarList().get(i).getId();
+			carData[1] = admin.getCarList().get(i).getBrand();
+			carData[2] = admin.getCarList().get(i).getGear();
+			carData[3] = admin.getCarList().get(i).getFuel();
+			carData[4] = admin.getCarList().get(i).getCapacity();
+			carData[5] = admin.getCarList().get(i).getDeposit();
+			carData[6] = admin.getCarList().get(i).getRental_fee();
+			carData[7] = admin.getCarList().get(i).getCount();
+			carModel.addRow(carData);
+		}
+		
+		setBackground(new Color(255, 204, 0));
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 1005, 642);
+		contentPane = new JPanel();
+		contentPane.setBackground(new Color(255, 204, 0));
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Merhaba, Sayın " + admin.getName());
+		lblNewLabel.setBounds(30, 30, 195, 16);
+		contentPane.add(lblNewLabel);
+		
+		JButton btnNewButton = new JButton("Çıkış Yap");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				LoginGUI1 login = new LoginGUI1();
+				login.setVisible(true);
+				dispose();
+			}
+		});
+		btnNewButton.setBackground(new Color(255, 255, 255));
+		btnNewButton.setBounds(30, 58, 117, 29);
+		contentPane.add(btnNewButton);
+		
+		JLabel lbl_admin_logo = new JLabel(new ImageIcon(getClass().getResource("projelogo.jpeg")));
+		lbl_admin_logo.setBounds(751, 6, 210, 147);
+		contentPane.add(lbl_admin_logo);
+		
+		JTabbedPane w_tab = new JTabbedPane(JTabbedPane.TOP);
+		w_tab.setBounds(6, 124, 993, 460);
+		contentPane.add(w_tab);
+		
+		JPanel w_admin = new JPanel();
+		w_tab.addTab("Araç Yönetimi", null, w_admin, null);
+		w_admin.setLayout(null);
+		
+		JLabel lblNewLabel_1 = new JLabel("Marka Model:");
+		lblNewLabel_1.setBounds(745, 6, 95, 16);
+		w_admin.add(lblNewLabel_1);
+		
+		fld_brand = new JTextField();
+		fld_brand.setBounds(745, 23, 221, 24);
+		w_admin.add(fld_brand);
+		fld_brand.setColumns(10);
+		
+		JLabel lblNewLabel_1_1 = new JLabel("Vites:");
+		lblNewLabel_1_1.setBounds(745, 45, 95, 16);
+		w_admin.add(lblNewLabel_1_1);
+		
+		fld_gear = new JTextField();
+		fld_gear.setColumns(10);
+		fld_gear.setBounds(745, 59, 221, 24);
+		w_admin.add(fld_gear);
+		
+		JLabel lblNewLabel_1_2 = new JLabel("Yakıt:");
+		lblNewLabel_1_2.setBounds(745, 84, 95, 16);
+		w_admin.add(lblNewLabel_1_2);
+		
+		fld_fuel = new JTextField();
+		fld_fuel.setColumns(10);
+		fld_fuel.setBounds(745, 99, 221, 24);
+		w_admin.add(fld_fuel);
+		
+		JLabel lblNewLabel_1_3 = new JLabel("Kapasite:");
+		lblNewLabel_1_3.setBounds(745, 124, 95, 16);
+		w_admin.add(lblNewLabel_1_3);
+		
+		fld_capacity = new JTextField();
+		fld_capacity.setColumns(10);
+		fld_capacity.setBounds(745, 141, 221, 24);
+		w_admin.add(fld_capacity);
+		
+		fld_deposit = new JTextField();
+		fld_deposit.setColumns(10);
+		fld_deposit.setBounds(745, 183, 221, 24);
+		w_admin.add(fld_deposit);
+		
+		JLabel lblNewLabel_1_3_1 = new JLabel("Depozito Ücreti:");
+		lblNewLabel_1_3_1.setBounds(745, 166, 102, 16);
+		w_admin.add(lblNewLabel_1_3_1);
+		
+		fld_rental_fee = new JTextField();
+		fld_rental_fee.setColumns(10);
+		fld_rental_fee.setBounds(745, 227, 221, 24);
+		w_admin.add(fld_rental_fee);
+		
+		JLabel lblNewLabel_1_3_2 = new JLabel("Kiralama Ücreti (1 Günlük):");
+		lblNewLabel_1_3_2.setBounds(745, 210, 194, 16);
+		w_admin.add(lblNewLabel_1_3_2);
+		
+		JScrollPane w_scrollAdmin = new JScrollPane();
+		w_scrollAdmin.setBounds(6, 5, 727, 403);
+		w_admin.add(w_scrollAdmin);
+		
+		table_cars = new JTable(carModel);
+		w_scrollAdmin.setViewportView(table_cars);
+		
+		JButton btn_addCar = new JButton("Araç Ekle");
+		btn_addCar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(fld_brand.getText().length() == 0 || fld_gear.getText().length() == 0 ||fld_fuel.getText().length() == 0 || fld_capacity.getText().length() == 0 || fld_deposit.getText().length() == 0 || fld_rental_fee.getText().length() == 0  || fld_car_count.getText().length() == 0) {
+					JOptionPane.showMessageDialog(null, "Lütfen Boş Alanları Doldurunuz.");
+				} else {
+					try {
+						boolean add = admin.addCar(fld_brand.getText(), fld_gear.getText(), fld_fuel.getText() ,Integer.parseInt(fld_capacity.getText()), Integer.parseInt(fld_deposit.getText()), Integer.parseInt(fld_rental_fee.getText()), Integer.parseInt(fld_car_count.getText()));
+						if(add) {
+							JOptionPane.showMessageDialog(null, "Ekleme başarılı.");
+							fld_brand.setText(null);
+							fld_gear.setText(null);
+							fld_fuel.setText(null);
+							fld_capacity.setText(null);
+							fld_deposit.setText(null);
+							fld_rental_fee.setText(null);
+							fld_car_count.setText(null);
+							updateCarList();
+						}
+					} catch (NumberFormatException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					} catch (SQLException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+					
+			}
+		}});
+		btn_addCar.setFont(new Font("Lucida Grande", Font.BOLD, 18));
+		btn_addCar.setBackground(new Color(251, 251, 251));
+		btn_addCar.setBounds(745, 310, 221, 26);
+		w_admin.add(btn_addCar);	
+		
+		JLabel lblNewLabel_2 = new JLabel("ID:");
+		lblNewLabel_2.setBounds(745, 336, 61, 16);
+		w_admin.add(lblNewLabel_2);
+		
+		fld_car_id = new JTextField();
+		fld_car_id.setBounds(745, 352, 219, 26);
+		w_admin.add(fld_car_id);
+		fld_car_id.setColumns(10);
+		
+		JButton btnNewButton_1 = new JButton("Araç Sil");
+		btnNewButton_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(fld_car_id.getText().length() == 0) {
+					JOptionPane.showMessageDialog(null, "Lütfen silmek istediğiniz aracın ID'sini girin.");
+				} else {
+					try {
+						boolean delete = admin.deleteCar(Integer.parseInt(fld_car_id.getText()));
+						if(delete) {
+							JOptionPane.showMessageDialog(null, "Araç Silindi");
+							fld_car_id.setText(null);
+							updateCarList();
+						}
+					} catch (NumberFormatException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					} catch (SQLException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+				}
+			}
+		});
+		btnNewButton_1.setFont(new Font("Lucida Grande", Font.BOLD, 18));
+		btnNewButton_1.setBounds(745, 381, 221, 26);
+		w_admin.add(btnNewButton_1);
+		
+		fld_car_count = new JTextField();
+		fld_car_count.setColumns(10);
+		fld_car_count.setBounds(745, 274, 221, 24);
+		w_admin.add(fld_car_count);
+		
+		JLabel lblNewLabel_1_3_2_1 = new JLabel("Araç Sayısı");
+		lblNewLabel_1_3_2_1.setBounds(745, 257, 194, 16);
+		w_admin.add(lblNewLabel_1_3_2_1);
+		
+		table_cars.getModel().addTableModelListener(new TableModelListener() {
+
+			@Override
+			public void tableChanged(TableModelEvent e) {
+				if(e.getType() == TableModelEvent.UPDATE) {
+					int selectID = Integer.parseInt(table_cars.getValueAt(table_cars.getSelectedRow(),0).toString());
+					String selectBrand = table_cars.getValueAt(table_cars.getSelectedRow(),1).toString();
+					String selectGear = table_cars.getValueAt(table_cars.getSelectedRow(),2).toString();
+					String selectFuel = table_cars.getValueAt(table_cars.getSelectedRow(),3).toString();
+					int setCapacity = Integer.parseInt(table_cars.getValueAt(table_cars.getSelectedRow(),4).toString());
+					int setDeposit = Integer.parseInt(table_cars.getValueAt(table_cars.getSelectedRow(),5).toString());
+					int setRental_fee = Integer.parseInt(table_cars.getValueAt(table_cars.getSelectedRow(),6).toString());
+					int setCount = Integer.parseInt(table_cars.getValueAt(table_cars.getSelectedRow(),7).toString());
+					try {
+						boolean update = admin.updateCar(selectID, selectBrand, selectGear, selectFuel, setCapacity, setDeposit, setRental_fee, setCount);
+						if(update) {
+							JOptionPane.showMessageDialog(null, "Araç Başarıyla Güncellendi");
+						}
+					} catch (SQLException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+				}
+			}
+			
+		});
+		
+		
+	}
+	
+	public void updateCarList() throws SQLException {
+		DefaultTableModel clearList = (DefaultTableModel) table_cars.getModel();
+		clearList.setRowCount(0);
+		for(int i = 0; i < admin.getCarList().size(); i++) {
+			carData[0] = admin.getCarList().get(i).getId();
+			carData[1] = admin.getCarList().get(i).getBrand();
+			carData[2] = admin.getCarList().get(i).getGear();
+			carData[3] = admin.getCarList().get(i).getFuel();
+			carData[4] = admin.getCarList().get(i).getCapacity();
+			carData[5] = admin.getCarList().get(i).getDeposit();
+			carData[6] = admin.getCarList().get(i).getRental_fee();
+			carData[7] = admin.getCarList().get(i).getCount();
+			carModel.addRow(carData);
+		}		
+	}
+}
---
+++ src/View1/LoginGUI1.java
@@ -0,0 +1,237 @@
+package View1;
+
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+import Helper.DBConnection;
+import Model.Admin;
+import Model.User;
+
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+
+import java.awt.Color;
+import javax.swing.ImageIcon;
+import java.awt.Font;
+import javax.swing.JTabbedPane;
+import javax.swing.JTextField;
+import javax.swing.JPasswordField;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.awt.event.ActionEvent;
+import javax.swing.SwingConstants;
+import java.awt.Window.Type;
+public class LoginGUI1 extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	private JPanel w_pane;
+	private JTextField fld_user_email;
+	private JPasswordField fld_user_pass;
+	private JTextField fld_admin_email;
+	private JPasswordField fld_admin_pass;
+	private DBConnection dbcon = new DBConnection();
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					LoginGUI1 frame = new LoginGUI1();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public LoginGUI1() {
+		setTitle("Rent Araç Kiralama");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 562, 470);
+		w_pane = new JPanel();
+		w_pane.setBackground(new Color(255, 204, 0));
+		w_pane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(w_pane);
+		w_pane.setLayout(null);
+		
+		JLabel lbl_logo = new JLabel(new ImageIcon(getClass().getResource("projelogo.jpeg")));
+		lbl_logo.setBounds(186, 0, 192, 160);
+		w_pane.add(lbl_logo);
+		
+		JLabel lblNewLabel = new JLabel("Rent Araç Kiralamaya Hoşgeldiniz");
+		lblNewLabel.setFont(new Font("Lucida Grande", Font.BOLD, 23));
+		lblNewLabel.setBounds(91, 150, 406, 49);
+		w_pane.add(lblNewLabel);
+		
+		JTabbedPane w_tabpane = new JTabbedPane(JTabbedPane.TOP);
+		w_tabpane.setTabLayoutPolicy(JTabbedPane.SCROLL_TAB_LAYOUT);
+		w_tabpane.setForeground(new Color(255, 255, 255));
+		w_tabpane.setBackground(new Color(0, 0, 0));
+		w_tabpane.setBounds(6, 211, 550, 225);
+		w_pane.add(w_tabpane);
+		
+		JPanel panel = new JPanel();
+		w_tabpane.addTab("Kullanıcı Girişi", null, panel, null);
+		panel.setBackground(new Color(226, 226, 226));
+		panel.setLayout(null);
+		
+		JLabel lblEposta = new JLabel("E-Posta:");
+		lblEposta.setBounds(23, 16, 83, 28);
+		lblEposta.setFont(new Font("Lucida Grande", Font.PLAIN, 20));
+		panel.add(lblEposta);
+		
+		JLabel lblsifre = new JLabel("Şifre:");
+		lblsifre.setFont(new Font("Lucida Grande", Font.PLAIN, 20));
+		lblsifre.setBounds(23, 65, 83, 28);
+		panel.add(lblsifre);
+		
+		fld_user_email = new JTextField();
+		fld_user_email.setBounds(118, 19, 251, 28);
+		panel.add(fld_user_email);
+		fld_user_email.setColumns(10);
+		
+		fld_user_pass = new JPasswordField();
+		fld_user_pass.setBounds(118, 69, 251, 26);
+		panel.add(fld_user_pass);
+		
+		JButton btn_user_login = new JButton("Giriş Yap");
+		btn_user_login.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(fld_user_email.getText().length() == 0 || fld_user_pass.getText().length() == 0) {
+					JOptionPane.showMessageDialog(null, "Lütfen boş alanları doldurunuz.");
+				} else {
+					try {
+						Connection con = dbcon.connDb();
+						Statement st = con.createStatement();
+						ResultSet rs = st.executeQuery("SELECT * FROM users");
+						boolean login = false;
+						while(rs.next()) {
+							if(fld_user_email.getText().equals(rs.getString("email")) && fld_user_pass.getText().equals(rs.getString("password"))) {
+								User user = new User();
+								user.setId(rs.getInt("id"));
+								user.setName(rs.getString("name"));
+								user.setSurname(rs.getString("surname"));
+								user.setEmail(rs.getString("email"));
+								user.setPassword(rs.getString("password"));
+								UserGUI1 userGUI = new UserGUI1(user);
+								userGUI.setVisible(true);
+								dispose();
+								login = true;
+								break;
+							}
+						}
+						if(!login) {
+							JOptionPane.showMessageDialog(null, "Girmiş olduğunuz e-posta veya parola yanlış.");
+							fld_user_email.setText(null);
+							fld_user_pass.setText(null);
+						}
+					} catch (SQLException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+
+					
+				}
+			}
+		});
+		btn_user_login.setBounds(43, 116, 209, 43);
+		panel.add(btn_user_login);
+		
+		JButton btn_user_register = new JButton("Kayıt Ol");
+		btn_user_register.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				User user = new User();
+				UserRegisterGUI1 userRegisterGUI = new UserRegisterGUI1(user);
+				userRegisterGUI.setVisible(true);
+				dispose();
+			}
+		});
+		btn_user_register.setBounds(275, 116, 209, 43);
+		panel.add(btn_user_register);
+		
+		JPanel panel_1 = new JPanel();
+		panel_1.setBackground(new Color(226, 226, 226));
+		w_tabpane.addTab("Admin Girişi", null, panel_1, null);
+		panel_1.setLayout(null);
+		
+		JLabel lblEposta_1 = new JLabel("E-Posta:");
+		lblEposta_1.setFont(new Font("Lucida Grande", Font.PLAIN, 20));
+		lblEposta_1.setBounds(22, 17, 83, 28);
+		panel_1.add(lblEposta_1);
+		
+		fld_admin_email = new JTextField();
+		fld_admin_email.setColumns(10);
+		fld_admin_email.setBounds(117, 20, 251, 28);
+		panel_1.add(fld_admin_email);
+		
+		JLabel lblsifre_1 = new JLabel("Şifre:");
+		lblsifre_1.setFont(new Font("Lucida Grande", Font.PLAIN, 20));
+		lblsifre_1.setBounds(22, 68, 83, 28);
+		panel_1.add(lblsifre_1);
+		
+		fld_admin_pass = new JPasswordField();
+		fld_admin_pass.setBounds(117, 72, 251, 26);
+		panel_1.add(fld_admin_pass);
+		
+		JButton btn_admin_login = new JButton("Giriş Yap");
+		btn_admin_login.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(fld_admin_email.getText().length() == 0 || fld_admin_pass.getText().length() == 0) {
+					JOptionPane.showMessageDialog(null, "Lütfen Boş Alanları Doldurunuz.");
+				} else {
+					try {
+						Connection con = dbcon.connDb();
+						Statement st = con.createStatement();
+						ResultSet rs = st.executeQuery("SELECT * FROM admins");
+						boolean login = false;
+						while(rs.next()) {
+							if(fld_admin_email.getText().equals(rs.getString("email")) && fld_admin_pass.getText().equals(rs.getString("password"))) {
+								Admin admin = new Admin();
+								admin.setId(rs.getInt("id"));
+								admin.setName(rs.getString("name"));
+								admin.setSurname(rs.getString("surname"));
+								admin.setEmail(rs.getString("email"));
+								admin.setPassword(rs.getString("password"));
+								AdminGUI1 adminGUI = new AdminGUI1(admin);
+								adminGUI.setVisible(true);
+								dispose();
+								login = true;
+								break;
+							}
+						}
+						if(!login) {
+							JOptionPane.showMessageDialog(null, "Girmiş olduğunuz e-posta veya parola yanlış.");
+							fld_admin_email.setText(null);
+							fld_admin_pass.setText(null);
+						}
+					} catch (SQLException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+				}
+			}
+		});
+		btn_admin_login.setBounds(22, 119, 476, 43);
+		panel_1.add(btn_admin_login);
+		
+		JLabel lblNewLabel_1 = new JLabel("Eğer hesabınız yoksa kayıt ol butonundan hesap oluşturabilirsiniz.");
+		lblNewLabel_1.setFont(new Font("Lucida Grande", Font.PLAIN, 17));
+		lblNewLabel_1.setBounds(6, 189, 550, 26);
+		w_pane.add(lblNewLabel_1);
+	}
+}
